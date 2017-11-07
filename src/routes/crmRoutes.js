a037ae5af88dc218f6473f22264161ee69a308ac
Roberto C Navarro <rnavarro70@gmail.com>
2017-11-07 11:44:10 -0600

    complete app

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,18 +1,30 @@
+import {
+    addNewContact,
+    getContacts,
+    getContactWithID,
+    updateContact,
+    deleteContact
+} from "../controllers/crmController";
+
 const routes = (app) => {
     app.route("/contact")
+        // GET endpoint
         .get((req, res, next) => {
             // middleware
             console.log(`Request from: ${req.originalUrl}`)
             console.log(`Request type: ${req.method}`)
-            next(); 
-        }, (req, res, next) => {
-            res.send("GET request successful!!");
-        })
-        .post((req, res) => res.send("POST request successful!!"));
+            next();
+        }, getContacts)
+        //POST endpoint
+        .post(addNewContact);
 
     app.route("/contact/:contactId")
-        .put((req, res) => res.send("PUT request successful!!"))
-        .delete((req, res) => res.send("DELETE request successful!!"));
+        // get specific contact by Id
+        .get(getContactWithID)
+
+        // update specific contact
+        .put(updateContact)
+        .delete(deleteContact);
 
 }
 
